5baebde519d0d6b2d2001ec69a637378d98b02a1
Kielan Sullivan <kielansean@gmail.com>
2018-05-30 14:21:43 -0500

    Added comments to email script

--- Weekly_Notes_Email.ps1
+++ Weekly_Notes_Email.ps1
@@ -1,3 +1,14 @@
+###########################################################
+# AUTHOR  : Kielan Sullivan
+# CREATED : 05-30-2018 
+# UPDATED :
+# COMMENT : This script is used for a windows cron job (schtask)
+#			When it i time to execute, read in a file containing 
+#			my notes for he week and emails them to me.  
+###########################################################
+
+
+
 $date = Get-Date -DisplayHint Date
 $file = "C:\Users\KielanSullivan\Desktop\Daily Log\DailyLog.txt"
 $body = (Get-Content $file | out-string)
